d650500d66e3a0bbe787fe9807a54c4d2847c078
neerajneeruz <good4pc@gmail.com>
2015-07-08 10:05:13 +0530

    Initial Commit

---
+++ cognalys/cognalys.h
@@ -0,0 +1,13 @@
+//
+//  cognalys.h
+//  cognalys
+//
+//  Created by MFluid Apps on 08/07/15.
+//  Copyright (c) 2015 Mfluid Mobile Apps Pvt. Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cognalys : NSObject
+
+@end
---
+++ cognalys/cognalys.m
@@ -0,0 +1,13 @@
+//
+//  cognalys.m
+//  cognalys
+//
+//  Created by MFluid Apps on 08/07/15.
+//  Copyright (c) 2015 Mfluid Mobile Apps Pvt. Ltd. All rights reserved.
+//
+
+#import "cognalys.h"
+
+@implementation cognalys
+
+@end
